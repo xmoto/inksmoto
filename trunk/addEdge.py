059bd0a6670f03a1855c223da3db07a0a1c23033
theonlydude <theonlydude@18dbd939-ec26-0410-991d-dbb253c97938>
2007-02-06 22:28:05 +0000

    can put two differents edge to a block (upper and down). checkbox to update only the desired side

--- trunk/addEdge.py
+++ trunk/addEdge.py
@@ -4,7 +4,15 @@ class AddEdge(XmotoExtension):
     def __init__(self):
         XmotoExtension.__init__(self)
         self.OptionParser.add_option("--texture", type="string", dest="texture", 
-                                     help="texture name")
+                                     help="upper texture name")
+        self.OptionParser.add_option("--downtexture", type="string", dest="downtexture",
+                                     help="down texture name")
+        self.OptionParser.add_option("--update_upper", type="string", dest="update_upper",
+                                     help="if true, update upper texture")
+        self.OptionParser.add_option("--update_down",  type="string", dest="update_down",
+                                     help="if true, update down texture")
+        self.OptionParser.add_option("--edge_description", type="string",
+                                     dest="edge_description", help="not used")
 
     def getLabelChanges(self):
         changes = []
@@ -12,8 +20,10 @@ class AddEdge(XmotoExtension):
         if self.label.has_key('typeid'):
             self.label.clear()
 
-        if self.options.texture not in ['', None]:
+        if self.options.update_upper == 'true' and self.options.texture not in ['', None]:
             changes.append(['edge', {'texture':self.options.texture}])
+        if self.options.update_down  == 'true' and self.options.downtexture not in ['', None]:
+            changes.append(['edge', {'downtexture':self.options.downtexture}])
 
         return changes
 

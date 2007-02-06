8e6f48a0e914196990d0dc1308c4e6f6da9374dc
theonlydude <theonlydude@18dbd939-ec26-0410-991d-dbb253c97938>
2007-02-06 22:36:54 +0000

    allow to remove the upper or down texture or both.

--- trunk/removeEdge.py
+++ trunk/removeEdge.py
@@ -3,10 +3,18 @@ from xmotoExtension import XmotoExtension
 class removeEdge(XmotoExtension):
     def __init__(self):
         XmotoExtension.__init__(self)
+        self.OptionParser.add_option("--remove_up", type="string", dest="remove_up",
+                                     help="if true, remove upper texture")
+        self.OptionParser.add_option("--remove_down",  type="string", dest="remove_down",
+                                     help="if true, remove down texture")
 
     def getLabelChanges(self):
-        if self.label.has_key('edge'):
-            del self.label['edge']
+        if self.options.remove_up == 'true' and self.label.has_key('edge'):
+            if self.label['edge'].has_key('texture'):
+                del self.label['edge']['texture']
+        if self.options.remove_down == 'true' and self.label.has_key('edge'):
+            if self.label['edge'].has_key('downtexture'):
+                del self.label['edge']['downtexture']
 
         return []
 

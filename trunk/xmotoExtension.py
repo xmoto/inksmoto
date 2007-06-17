2b88d448824ef9fa0f0feff221e98560bb86726b
theonlydude <theonlydude@18dbd939-ec26-0410-991d-dbb253c97938>
2007-06-17 21:49:51 +0000

    Make zones transparent.  Modulate block colors.

--- trunk/xmotoExtension.py
+++ trunk/xmotoExtension.py
@@ -6,6 +6,20 @@ import base64
 import logging, log
 import os
 
+def getInkscapeExtensionsDir():
+    system = os.name
+    if system == 'nt':
+        # check this value from a Windows machine
+        userDir = expanduser('~/Application Data/Inkscape/extensions')
+
+        # if the userDir exists, use it. else, use the appsDir
+        if isdir(userDir):
+            return userDir
+        else:
+            return join(os.getcwd(), "share/extensions")
+    else:
+        return expanduser('~/.inkscape/extensions')
+
 class XmotoExtension(Effect):
     """
     if you want to change and manipulate values, use getXXXXXChanges() in your child class
@@ -14,20 +28,6 @@ class XmotoExtension(Effect):
     def __init__(self):
         Effect.__init__(self)
         self.patterns = {}
-
-    def getInkscapeExtensionsDir(self):
-        system = os.name
-        if system == 'nt':
-            # check this value from a Windows machine
-            userDir = expanduser('~/Application Data/Inkscape/extensions')
-
-            # if the userDir exists, use it. else, use the appsDir
-            if isdir(userDir):
-                return userDir
-            else:
-                return join(os.getcwd(), "share/extensions")
-        else:
-            return expanduser('~/.inkscape/extensions')
 
     def getPatterns(self):
         patterns = self.document.getElementsByTagName('patterns')
@@ -56,7 +56,7 @@ class XmotoExtension(Effect):
                                 ('id', 'pattern_%s' % textureName)]:
                 pattern.setAttribute(name, value)
             image = xml.dom.Element.Element(self.defs.ownerDocument, 'image', None, None, None)
-            imageAbsURL = self.getInkscapeExtensionsDir() + '/%s' % texture['file']
+            imageAbsURL = getInkscapeExtensionsDir() + '/%s' % texture['file']
             imageFile   = open(imageAbsURL, 'rb').read()
             for name, value in [('xlink:href', 'data:image/%s;base64,%s' % (texture['file'][texture['file'].rfind('.')+1:],
                                                                             base64.encodestring(imageFile))),
@@ -152,6 +152,7 @@ class XmotoExtension(Effect):
                 self.style['fill'] = 'gray'
             elif typeid == 'Zone':
                 self.style['fill'] = 'cyan'
+                self.style['fill-opacity'] = 0.5
             else:
                 self.style['fill'] = 'black'
         else:
@@ -159,13 +160,41 @@ class XmotoExtension(Effect):
             #        patternId = self.addPattern(self.options.texture, textures)
             #        return [('fill', 'url(#%s)' % patternId)]
 
-            # normal block
-            self.style['fill'] = 'mediumaquamarine'
+            def generateBlockColor(r, r2, g, g2, b, b2):
+                # randomly change the color to distinguish between adjacent blocks
+                from random import randint
+                def dec2hex(d):
+                    convert = {0:'0', 1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8', 9:'9', 10:'a', 11:'b', 12:'c', 13:'d', 14:'e', 15:'f'}
+                    return convert[d]
+
+                def hex2dec(x):
+                    convert = {'0':0, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'a':10, 'b':11, 'c':12, 'd':13, 'e':14, 'f':15}
+                    return convert[x]
+
+                # r, g and b must not be 'f' before adding the random int or it could became '0'
+                r = (hex2dec(r) + randint(0,1)) % 16
+                g = (hex2dec(g) + randint(0,1)) % 16
+                b = (hex2dec(b) + randint(0,1)) % 16
+                chaos = (randint(0, 15), randint(0, 15), randint(0, 15))
+                return '#' + dec2hex(r) + r2 + dec2hex(g) + g2 + dec2hex(b) + b2
+
+            self.style['fill-opacity'] = '1'
             if self.label.has_key('position'):
-                if self.label['position'].has_key('background'):
-                    self.style['fill'] = 'darkkhaki'
+                if self.label['position'].has_key('background') and self.label['position'].has_key('dynamic'):
+                    # d36b00
+                    self.style['fill'] = generateBlockColor('d', '3', '6', 'b', '0', '0')
+                elif self.label['position'].has_key('background'):
+                    # bdb76b = darkkhaki
+                    self.style['fill'] = generateBlockColor('b', 'd', 'b', '7', '6', 'b')
                 elif self.label['position'].has_key('dynamic'):
-                    self.style['fill'] = 'lightcoral'
+                    # f08080 = lightcoral
+                    self.style['fill'] = generateBlockColor('e', '0', '8', '0', '8', '0')
+                else:
+                    # 66cdaa = mediumaquamarine
+                    self.style['fill'] = generateBlockColor('6', '6', 'c', 'd', 'a', 'a')
+            else:
+                # 66cdaa = mediumaquamarine
+                self.style['fill'] = generateBlockColor('6', '6', 'c', 'd', 'a', 'a')
 
             if self.label.has_key('edge'):
                 self.style['stroke-width']    = '1px'

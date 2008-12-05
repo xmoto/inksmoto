3af0565a3e54b30294a011064d95364f5b8d639f
theonlydude <theonlydude@18dbd939-ec26-0410-991d-dbb253c97938>
2008-12-05 18:25:34 +0000

    add inksmoto .deb creation script and control file

---
+++ trunk/deb/create_package.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+INSTALL_DIR="/usr/share/inkscape/extensions"
+VERSION=`grep "Version" control-install | cut -d ' ' -f 2`
+
+mkdir -p inksmoto/DEBIAN
+cp control-install inksmoto/DEBIAN/control
+
+mkdir -p "inksmoto"$INSTALL_DIR
+cp -rf ../extensions/* "inksmoto"$INSTALL_DIR
+# this are two inkscape files.
+rm -f "inksmoto""$INSTALL_DIR""/inkex.py"
+rm -f "inksmoto""$INSTALL_DIR""/bezmisc.py"
+
+dpkg-deb --build inksmoto inksmoto-$VERSION.deb
+rm -rf inksmoto

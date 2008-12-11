1ffc76970babc09668ebcbffaa7feb371ffc25b0
theonlydude <theonlydude@18dbd939-ec26-0410-991d-dbb253c97938>
2008-12-11 21:15:11 +0000

    update deb creation script with new paths

--- packaging/deb/create_package.sh
+++ packaging/deb/create_package.sh
@@ -6,14 +6,13 @@ mkdir -p inksmoto/DEBIAN
 cp control-install inksmoto/DEBIAN/control
 
 mkdir -p "inksmoto"$INSTALL_DIR
-cp -rf ../extensions/* "inksmoto"$INSTALL_DIR
+cp -rf ../../extensions/* "inksmoto"$INSTALL_DIR
 # this are two inkscape files.
 rm -f "inksmoto""$INSTALL_DIR""/inkex.py"
 rm -f "inksmoto""$INSTALL_DIR""/bezmisc.py"
+# remove emacs backup and pyc files
 rm -f "inksmoto""$INSTALL_DIR""/*~"
 rm -f "inksmoto""$INSTALL_DIR""/*.pyc"
-cp ~/.inkscape/extensions/listAvailableElements.xml "inksmoto""$INSTALL_DIR"
-cp ~/.inkscape/extensions/listAvailableElements.py "inksmoto""$INSTALL_DIR"
 
 dpkg-deb --build inksmoto inksmoto-$VERSION.deb
 rm -rf inksmoto

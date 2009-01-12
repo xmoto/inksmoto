6b2e4ae568b909a5730ff7bd8002642402108e3f
theonlydude <theonlydude@18dbd939-ec26-0410-991d-dbb253c97938>
2009-01-12 20:28:22 +0000

    remove wrong tag

--- tags/inksmoto_0.5.0~rc2/extensions/addStrawberry.py
+++
@@ -1,9 +0,0 @@
-from addEntity import AddEntity
-
-class AddStrawberry(AddEntity):
-    def __init__(self):
-        AddEntity.__init__(self)
-        self.typeid = 'Strawberry'
-
-e = AddStrawberry()
-e.affect()
--- tags/inksmoto_0.5.0~rc2/extensions/addWrecker.py
+++
@@ -1,9 +0,0 @@
-from addEntity import AddEntity
-
-class AddWrecker(AddEntity):
-    def __init__(self):
-        AddEntity.__init__(self)
-        self.typeid = 'Wrecker'
-    
-e = AddWrecker()
-e.affect()

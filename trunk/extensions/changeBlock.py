2b937030d7f7348b3433a1275a96096bcd12a17d
theonlydude <theonlydude@18dbd939-ec26-0410-991d-dbb253c97938>
2009-01-25 19:23:26 +0000

    pylint in progress

--- trunk/extensions/changeBlock.py
+++ trunk/extensions/changeBlock.py
@@ -1,15 +1,13 @@
-from xmotoExtensionTkinter import XmotoExtTkElement, XmotoListbox, XmotoScale, XmotoEntry, XmotoLabel, XmotoCheckBox
+from xmotoExtensionTkinter import XmotoExtTkElement, XmotoScale, XmotoCheckBox
 from xmotoTools import createIfAbsent, delWithoutExcept
-from listAvailableElements import textures, edgeTextures
-import Tkinter
 
 class ChangeBlock(XmotoExtTkElement):
     def __init__(self):
         XmotoExtTkElement.__init__(self)
-        self.defaultGrip  = 20.0
-        self.defaultMass  = 30.0
-        self.defaultElasticity = 0.0
-        self.defaultFriction = 0.5
+        self.defGrip  = 20.0
+        self.defMass  = 30.0
+        self.defElasticity = 0.0
+        self.defFriction = 0.5
         self.namespacesInCommon = ['position', 'physics']
 
     def getUserChanges(self):
@@ -20,14 +18,18 @@ class ChangeBlock(XmotoExtTkElement):
 
         # handle position
         createIfAbsent(self.commonValues, 'position')
-        self.setOrDelBool(self.commonValues, 'position', self.background, 'background')
-        self.setOrDelBool(self.commonValues, 'position', self.dynamic,    'dynamic')
-        self.setOrDelBool(self.commonValues, 'position', self.physics,    'physics')
+        self.setOrDelBool(self.commonValues, 'position',
+                          self.background, 'background')
+        self.setOrDelBool(self.commonValues, 'position',
+                          self.dynamic, 'dynamic')
+        self.setOrDelBool(self.commonValues, 'position',
+                          self.physics, 'physics')
 
         createIfAbsent(self.commonValues, 'physics')
         self.commonValues['physics']['grip'] = self.grip.get()
         if 'physics' in self.commonValues['position']:
-            self.setOrDelBool(self.commonValues, 'physics', self.infinity, 'infinitemass')
+            self.setOrDelBool(self.commonValues, 'physics',
+                              self.infinity, 'infinitemass')
             self.commonValues['physics']['mass']       = self.mass.get()
             self.commonValues['physics']['elasticity'] = self.elasticity.get()
             self.commonValues['physics']['friction']   = self.friction.get()
@@ -42,21 +44,59 @@ class ChangeBlock(XmotoExtTkElement):
 
         # type
         self.defineTitle(self.frame, "Type")
-        self.defineLabel(self.frame, "Uncheck all to convert into normal block.")
-        self.background = XmotoCheckBox(self.frame, self.getValue(self.commonValues, 'position', 'background'), text='Background block')
-        self.dynamic    = XmotoCheckBox(self.frame, self.getValue(self.commonValues, 'position', 'dynamic'),    text='Dynamic block')
-        self.physics    = XmotoCheckBox(self.frame, self.getValue(self.commonValues, 'position', 'physics'),    text='Physics block', command=self.physicsCallback)
+        label = "Uncheck all to convert into normal block."
+        self.defineLabel(self.frame, label)
+        self.background = XmotoCheckBox(self.frame,
+                                        self.getValue(self.commonValues,
+                                                      'position',
+                                                      'background'),
+                                        text='Background block')
+        self.dynamic = XmotoCheckBox(self.frame,
+                                     self.getValue(self.commonValues,
+                                                   'position',
+                                                   'dynamic'),
+                                     text='Dynamic block')
+        self.physics = XmotoCheckBox(self.frame,
+                                     self.getValue(self.commonValues,
+                                                   'position',
+                                                   'physics'),
+                                     text='Physics block',
+                                     command=self.physicsCallback)
 
         # physic
         self.defineTitle(self.frame, "Ode Physic")
         self.defineLabel(self.frame, "The grip with the bike wheels")
-        self.grip = XmotoScale(self.frame, self.getValue(self.commonValues, 'physics', 'grip', default=self.defaultGrip), label='Grip', from_=0, to=50, resolution=1, default=self.defaultGrip)
+        self.grip = XmotoScale(self.frame,
+                               self.getValue(self.commonValues, 'physics',
+                                             'grip', default=self.defGrip),
+                               label='Grip', from_=0, to=50,
+                               resolution=1, default=self.defGrip)
 
         self.defineTitle(self.frame, "Chipmunk Physic")
-        self.infinity = XmotoCheckBox(self.frame, self.getValue(self.commonValues, 'physics', 'infinitemass'), text='Infinite Mass')
-        self.mass = XmotoScale(self.frame, self.getValue(self.commonValues, 'physics', 'mass', default=self.defaultMass), label='Mass', from_=1, to=1000, resolution=1, default=self.defaultMass)
-        self.elasticity = XmotoScale(self.frame, self.getValue(self.commonValues, 'physics', 'elasticity', default=self.defaultElasticity), label='Elasticity', from_=0.0, to=1.0, resolution=0.1, default=self.defaultElasticity)
-        self.friction = XmotoScale(self.frame, self.getValue(self.commonValues, 'physics', 'friction', default=self.defaultFriction), label='Friction', from_=0.0, to=1.0, resolution=0.1, default=self.defaultFriction)
+        self.infinity = XmotoCheckBox(self.frame,
+                                      self.getValue(self.commonValues,
+                                                    'physics',
+                                                    'infinitemass'),
+                                      text='Infinite Mass')
+        self.mass = XmotoScale(self.frame,
+                               self.getValue(self.commonValues, 'physics',
+                                             'mass', default=self.defMass),
+                               label='Mass', from_=1, to=1000,
+                               resolution=1, default=self.defMass)
+        self.elasticity = XmotoScale(self.frame,
+                                     self.getValue(self.commonValues,
+                                                   'physics',
+                                                   'elasticity',
+                                                   default=self.defElasticity),
+                                     label='Elasticity', from_=0.0, to=1.0,
+                                     resolution=0.1, default=self.defElasticity)
+        self.friction = XmotoScale(self.frame,
+                                   self.getValue(self.commonValues,
+                                                 'physics',
+                                                 'friction',
+                                                 default=self.defFriction),
+                                   label='Friction', from_=0.0, to=1.0,
+                                   resolution=0.1, default=self.defFriction)
         self.physicsCallback()
 
     def physicsCallback(self):
--- trunk/extensions/changeBlockTexture.py
+++ trunk/extensions/changeBlockTexture.py
@@ -1,4 +1,5 @@
-from xmotoExtensionTkinter import XmotoExtTkElement, XmotoListbox, XmotoScale, XmotoEntry, XmotoBitmap, XmotoLabel
+from xmotoExtensionTkinter import XmotoExtTkElement, XmotoScale
+from xmotoExtensionTkinter import XmotoBitmap, XmotoLabel
 from xmotoTools import createIfAbsent, delWithoutExcept, notSetBitmap
 from listAvailableElements import textures, edgeTextures
 import Tkinter
@@ -6,9 +7,9 @@ import Tkinter
 class ChangeBlock(XmotoExtTkElement):
     def __init__(self):
         XmotoExtTkElement.__init__(self)
-        self.defaultScale = 1.0
-        self.defaultAngle = 270.0
-        self.defaultMethod = 'angle'
+        self.defScale = 1.0
+        self.defAngle = 270.0
+        self.defMethod = 'angle'
         self.namespacesInCommon = ['usetexture', 'edge', 'edges']
 
     def getUserChanges(self):
@@ -25,31 +26,35 @@ class ChangeBlock(XmotoExtTkElement):
             raise Exception('You have to give a texture to the block')
 
         self.setOrDelBitmap(self.commonValues, 'usetexture', 'id', self.texture)
-        if self.scale.get() != self.defaultScale:
+        if self.scale.get() != self.defScale:
             self.commonValues['usetexture']['scale'] = self.scale.get()
 
         # handle edges
         createIfAbsent(self.commonValues, 'edge')
         createIfAbsent(self.commonValues, 'edges')
 
-        if self.drawMethod.get() != self.defaultMethod:
+        if self.drawMethod.get() != self.defMethod:
             self.commonValues['edges']['drawmethod'] = self.drawMethod.get()
         if self.drawMethod.get() in ['angle']:
-            if self.angle.get() != self.defaultAngle:
+            if self.angle.get() != self.defAngle:
                 self.commonValues['edges']['angle'] = self.angle.get()
 
-            self.setOrDelBitmap(self.commonValues, 'edge', 'texture',     self.upperEdge)
-            self.setOrDelBitmap(self.commonValues, 'edge', 'downtexture', self.downEdge)
+            self.setOrDelBitmap(self.commonValues, 'edge',
+                                'texture',     self.upperEdge)
+            self.setOrDelBitmap(self.commonValues, 'edge',
+                                'downtexture', self.downEdge)
 
             # no edge texture selected
-            if 'texture' not in self.commonValues['edge'] and 'downtexture' not in self.commonValues['edge']:
+            if ('texture' not in self.commonValues['edge']
+                and 'downtexture' not in self.commonValues['edge']):
                 delWithoutExcept(self.commonValues, 'edge')
                 delWithoutExcept(self.commonValues, 'edges')
 
         elif self.drawMethod.get() in ['in', 'out']:
             delWithoutExcept(self.commonValues['edges'], 'angle')
             delWithoutExcept(self.commonValues['edge'],  'downtexture')
-            self.setOrDelBitmap(self.commonValues, 'edge', 'texture', self.upperEdge)
+            self.setOrDelBitmap(self.commonValues, 'edge',
+                                'texture', self.upperEdge)
 
             # no edge texture selected
             if 'texture' not in self.commonValues['edge']:
@@ -64,29 +69,59 @@ class ChangeBlock(XmotoExtTkElement):
         # texture
         self.defineTitle(self.frame, "Texture")
         self.defineLabel(self.frame, "Click the texture to choose another one.")
-        defaultTexture  = self.getValue(self.commonValues, 'usetexture', 'id', default='_None_')
-        self.texture = XmotoBitmap(self.frame, textures[defaultTexture]['file'], defaultTexture, self.textureSelectionWindow, buttonName='texture')
-        self.scale = XmotoScale(self.frame, self.getValue(self.commonValues, 'usetexture', 'scale', default=self.defaultScale), label='Scale', from_=0.1, to=10, resolution=0.1, default=self.defaultScale)
+        defaultTexture  = self.getValue(self.commonValues, 'usetexture',
+                                        'id', default='_None_')
+        self.texture = XmotoBitmap(self.frame, textures[defaultTexture]['file'],
+                                   defaultTexture, self.textureSelectionWindow,
+                                   buttonName='texture')
+        self.scale = XmotoScale(self.frame,
+                                self.getValue(self.commonValues, 'usetexture',
+                                              'scale', default=self.defScale),
+                                label='Scale', from_=0.1, to=10,
+                                resolution=0.1, default=self.defScale)
 
         # edges
         self.defineTitle(self.frame, "Edge")
         self.defineLabel(self.frame, "Edge drawing behaviour:")
-        buttons = [('using the given angle', 'angle'), ('inside the block', 'in'), ('outside the block', 'out')]
-        self.drawMethod = self.defineRadioButtons(self.frame, self.getValue(self.commonValues, 'edges', 'drawmethod', default='angle'),
-                                                  buttons, command=self.edgeDrawCallback)
+        buttons = [('using the given angle', 'angle'),
+                   ('inside the block', 'in'),
+                   ('outside the block', 'out')]
+        self.drawMethod = self.defineRadioButtons(self.frame,
+                                                  self.getValue(self.commonValues,
+                                                                'edges',
+                                                                'drawmethod',
+                                                                default='angle'),
+                                                  buttons,
+                                                  command=self.edgeDrawCallback)
 
         self.edgeFrame = Tkinter.Frame(self.frame)
-        defaultEdge    = self.getValue(self.commonValues, 'edge', 'texture', default='_None_')
+        defaultEdge    = self.getValue(self.commonValues, 'edge',
+                                       'texture', default='_None_')
         self.defineLabel(self.edgeFrame, "Upper edge texture", grid=(0, 0))
-        self.upperEdge = XmotoBitmap(self.edgeFrame, edgeTextures[defaultEdge]['file'], defaultEdge, self.edgeSelectionWindow, grid=(0, 1), buttonName='upperEdge')
+        self.upperEdge = XmotoBitmap(self.edgeFrame,
+                                     edgeTextures[defaultEdge]['file'],
+                                     defaultEdge, self.edgeSelectionWindow,
+                                     grid=(0, 1), buttonName='upperEdge')
 
-        defaultDownEdge= self.getValue(self.commonValues, 'edge', 'downtexture', default='_None_')
-        self.downEdgeLabel = XmotoLabel(self.edgeFrame, "Down edge texture", grid=(1, 0))
-        self.downEdge      = XmotoBitmap(self.edgeFrame, edgeTextures[defaultDownEdge]['file'], defaultDownEdge, self.edgeSelectionWindow, grid=(1, 1), buttonName='downEdge')
+        defaultDownEdge = self.getValue(self.commonValues, 'edge',
+                                        'downtexture', default='_None_')
+        self.downEdgeLabel = XmotoLabel(self.edgeFrame,
+                                        "Down edge texture",
+                                        grid=(1, 0))
+        self.downEdge      = XmotoBitmap(self.edgeFrame,
+                                         edgeTextures[defaultDownEdge]['file'],
+                                         defaultDownEdge,
+                                         self.edgeSelectionWindow,
+                                         grid=(1, 1), buttonName='downEdge')
         self.edgeFrame.pack()
 
-        self.angleLabel = self.defineLabel(self.frame, "Angle the edges point to (defaulted to 270.0):")
-        self.angle = XmotoScale(self.frame, self.getValue(self.commonValues, 'edges', 'angle', default=self.defaultAngle), label='Edge angle', from_=0, to=360, resolution=45, default=self.defaultAngle)
+        label = "Angle the edges point to (defaulted to 270.0):"
+        self.angleLabel = self.defineLabel(self.frame, label)
+        self.angle = XmotoScale(self.frame,
+                                self.getValue(self.commonValues, 'edges',
+                                              'angle', default=self.defAngle),
+                                label='Edge angle', from_=0, to=360,
+                                resolution=45, default=self.defAngle)
 
         # to update disabled buttons
         self.edgeDrawCallback()
@@ -107,7 +142,7 @@ class ChangeBlock(XmotoExtTkElement):
         elif buttonName in ['downEdge', 'upperEdge']:
             bitmapDict = edgeTextures
 
-        values = self.__dict__[buttonName].update(imgName, bitmapDict)
+        self.__dict__[buttonName].update(imgName, bitmapDict)
 
 def run():
     e = ChangeBlock()

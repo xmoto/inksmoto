5bf8c4ed399c7777cae07275ce39f2484537e39f
theonlydude <theonlydude@18dbd939-ec26-0410-991d-dbb253c97938>
2009-01-25 19:23:26 +0000

    pylint in progress

--- extensions/block_element.py
+++ extensions/block_element.py
@@ -4,14 +4,11 @@ from vector   import Vector
 from bezier   import Bezier
 from elements import Element
 from parametricArc  import ParametricArc
-from xmotoTools import getValue, createIfAbsent
+from xmotoTools import getValue, createIfAbsent, delWithoutExcept
 from math     import fabs
-import logging, log
+import logging
 
 class Block(Element):
-    def __init__(self, *args, **keywords):
-        Element.__init__(self, *args, **keywords)
-
     def writeBlockHead(self):
         self.content.append("\t<block id=\"%s\">" % self.curBlock)
         self.addElementParams()
@@ -39,7 +36,8 @@ class Block(Element):
 
         createIfAbsent(self.infos, 'position')
 
-        if 'x' not in self.infos['position'] or 'y' not in self.infos['position']:
+        if ('x' not in self.infos['position']
+            or 'y' not in self.infos['position']):
             self.infos['position']['x'] = '%f' % (-self.newWidth/2.0)
             self.infos['position']['y'] = '%f' % (self.newHeight/2.0)
 
@@ -53,20 +51,16 @@ class Block(Element):
             removeNonNormal(self.infos['position'])
         else:
             self.infos['position']['islayer'] = "true"
-            self.infos['position']['layerid'] = str(level.layerBlock2Level[layerNumber])
+            lid = str(level.layerBlock2Level[layerNumber])
+            self.infos['position']['layerid'] = lid
             removeNonNormal(self.infos['position'])
 
         if 'usetexture' not in self.infos:
             self.infos['usetexture'] = {'id':'default'}
             
-        self.edgeTexture = ""
-        self.downEdgeTexture = ""
-        if 'edge' in self.infos:
-            if 'texture' in self.infos['edge']:
-                self.edgeTexture = self.infos['edge']['texture']
-            if 'downtexture' in self.infos['edge']:
-                self.downEdgeTexture = self.infos['edge']['downtexture']
-            del self.infos['edge']
+        self.edgeTexture = getValue(self.infos, 'edge', 'texture', '')
+        self.downEdgeTexture = getValue(self.infos, 'edge', 'downtexture', '')
+        delWithoutExcept(self.infos, 'edge')
 
         if 'physics' in self.infos:
             if 'infinitemass' in self.infos['physics']:
@@ -93,75 +87,84 @@ class Block(Element):
 
         return self.content
 
-
-    def generateBezierCurvePoints(self, point1, controlPoint1, controlPoint2, point2):
-        return Bezier((point1, controlPoint1, controlPoint2, point2)).splitCurve()
-
-    def generateParametricArcPoints(self, (x1, y1), (x2, y2), (rx, ry), x_rot, fA, fS):
-        return ParametricArc((x1, y1), (x2, y2), (rx, ry), x_rot, fA, fS).splitArc()
-    
-    def preProcessVertex(self):
-        # apply transformations on block vertex
-        self.vertex = Factory().createObject('path_parser').parse(self.vertex)
-
-        # first, check if the path is a circle
-        # M x y
-        # A rx ry 0 1 1 x1 y
-        # A rx ry 0 1 1 x y
-        # Z
-	if len(self.vertex) == 4:
-            if self.vertex[0][0] == 'M' and self.vertex[1][0] == 'A' and self.vertex[2][0] == 'A' and self.vertex[3][0] == 'Z':
-                (element, values) = self.vertex[0]
+    def isCircle(self):
+        """ M x y
+         A rx ry 0 1 1 x1 y
+         A rx ry 0 1 1 x y
+         Z """
+        if len(self.vertex) == 4:
+            if (self.vertex[0][0] == 'M' and self.vertex[1][0] == 'A'
+                and self.vertex[2][0] == 'A' and self.vertex[3][0] == 'Z'):
+                values = self.vertex[0][1]
                 (x, y) = values['x'], values['y']
 
-                (element, values) = self.vertex[1]
-                (rx1, ry1, x1, y1) = values['rx'], values['ry'], values['x'], values['y']
+                values = self.vertex[1][1]
+                (rx1, ry1) = values['rx'], values['ry']
+                y1 = values['y']
 
-                (element, values) = self.vertex[2]
-                (rx2, ry2, x2, y2) = values['rx'], values['ry'], values['x'], values['y']
+                values = self.vertex[2][1]
+                (rx2, ry2) = values['rx'], values['ry']
+                (x2, y2) = values['x'], values['y']
 
-                if rx1 == rx2 and ry1 == ry2 and x2 == x and y1 == y and y2 == y:
-                    (element, values) = self.vertex[1]
-                    (xAxRot, laFlag, sFlag) = values['x_axis_rotation'], values['large_arc_flag'], values['sweep_flag']
+                if (rx1 == rx2 and ry1 == ry2
+                    and x2 == x and y1 == y and y2 == y):
+                    values = self.vertex[1][1]
+                    (xAxRot, laFlag, sFlag) = (values['x_axis_rotation'],
+                                               values['large_arc_flag'],
+                                               values['sweep_flag'])
 
                     if xAxRot == 0 and laFlag == 1 and sFlag == 1:
-                        (element, values) = self.vertex[2]
-                        (xAxRot, laFlag, sFlag) = values['x_axis_rotation'], values['large_arc_flag'], values['sweep_flag']
+                        values = self.vertex[2][1]
+                        (xAxRot, laFlag, sFlag) = (values['x_axis_rotation'],
+                                                   values['large_arc_flag'],
+                                                   values['sweep_flag'])
 
                         if xAxRot == 0 and laFlag == 1 and sFlag == 1:
                             # we have a circle
                             self.infos['collision'] = {}
                             self.infos['collision']['type'] = 'Circle'
                             self.infos['collision']['radius'] = 0.0
+        
+
+
+    def preProcessVertex(self):
+        """ apply transformations on block vertex """
+        self.vertex = Factory().createObject('path_parser').parse(self.vertex)
+
+        self.isCircle()
 
         # as we add new vertex, we need a new list to store them
         tmp = []
+        (lastX, lastY) = (0, 0)
         for element, valuesDic in self.vertex:
             if element == 'C':
-                x1, y1 = valuesDic['x1'], valuesDic['y1']
-                x2, y2 = valuesDic['x2'], valuesDic['y2']
-                x,  y  = valuesDic['x'],  valuesDic['y']
-                tmp.extend(self.generateBezierCurvePoints((self.lastX, self.lastY), (x1, y1), (x2, y2), (x, y)))
+                tmp.extend(Bezier(((lastX, lastY),
+                                   (valuesDic['x1'], valuesDic['y1']),
+                                   (valuesDic['x2'], valuesDic['y2']),
+                                   (valuesDic['x'],  valuesDic['y'])
+                                   )
+                                  ).splitCurve())
             elif element == 'A':
-                x,  y  = valuesDic['x'],  valuesDic['y']
-                rx, ry = valuesDic['rx'], valuesDic['ry']
-                tmp.extend(self.generateParametricArcPoints((self.lastX, self.lastY), (x, y), (rx,ry),
-                                                            valuesDic['x_axis_rotation'], 
-                                                            valuesDic['large_arc_flag'], 
-                                                            valuesDic['sweep_flag']))
+                tmp.extend(ParametricArc((lastX, lastY),
+                                         (valuesDic['x'],  valuesDic['y']),
+                                         (valuesDic['rx'], valuesDic['ry']),
+                                         valuesDic['x_axis_rotation'], 
+                                         valuesDic['large_arc_flag'], 
+                                         valuesDic['sweep_flag']).splitArc())
             else:
                 tmp.append([element, valuesDic])
 
             if valuesDic is not None:
-                self.lastX = valuesDic['x']
-                self.lastY = valuesDic['y']
+                lastX = valuesDic['x']
+                lastY = valuesDic['y']
         
         # apply transformation on the block
         self.aabb.reinit()
-        for line, lineDic in tmp:
+        for (line, lineDic) in tmp:
             if lineDic is not None:
-                x, y = self.applyRatioAndTransformOnPoint(lineDic['x'],  lineDic['y'])
-                lineDic['x'], lineDic['y']  = x, y
+                (x, y) = (lineDic['x'], lineDic['y'])
+                (x, y) = self.applyRatioAndTransformOnPoint(x, y)
+                (lineDic['x'], lineDic['y']) = (x, y)
                 self.aabb.addPoint(x, y)
 
         self.vertex = tmp
@@ -173,21 +176,16 @@ class Block(Element):
         oldPosy = float(self.infos['position']['y'])
         self.xDiff = posx - oldPosx
         self.yDiff = posy - oldPosy
-        self.infos['position']['x'] = '%f' % (posx)
-        self.infos['position']['y'] = '%f' % (posy)
+        self.infos['position']['x'] = str(posx)
+        self.infos['position']['y'] = str(posy)
 
         if 'collision' in self.infos:
             self.infos['collision']['radius'] = self.aabb.width() / 2.0
 
-    def initBlockInfos(self):
-        self.lastx = 99999
-        self.lasty = 99999
-
     def writeBlockVertex(self):
         # some block got no final 'z'...
         ret = False
-        self.currentBlockVertex = []
-        self.initBlockInfos()
+        self.curBlockVertex = []
         self.aabb.reinit()
 
         while len(self.vertex) != 0:
@@ -206,30 +204,34 @@ class Block(Element):
         
         # if the last vertex is the same as the first, xmoto crashes
         def sameVertex(v1, v2):
-            return (abs(v1[0] - v2[0]) < 0.00001) and (abs(v1[1] - v2[1]) < 0.00001)
+            return ((abs(v1[0] - v2[0]) < 0.00001)
+                    and (abs(v1[1] - v2[1]) < 0.00001))
 
-        if sameVertex(self.currentBlockVertex[0], self.currentBlockVertex[-1]):
-            self.currentBlockVertex = self.currentBlockVertex[:-1]
+        if sameVertex(self.curBlockVertex[0], self.curBlockVertex[-1]):
+            self.curBlockVertex = self.curBlockVertex[:-1]
             logging.debug("%s: remove last vertex" % self.curBlock)
         
         # need at least 3 vertex in a block
-        if len(self.currentBlockVertex) < 3:
-            logging.info("A block need at least three vertex (block %s)" % (self.curBlock))
+        if len(self.curBlockVertex) < 3:
+            logging.info("A block need at least three vertex (block %s)"
+                         % (self.curBlock))
             return ret
 
         self.addBlockEdge()
 
-        for (x,y,edge) in self.currentBlockVertex:
-            if edge:
+        wEdge = "\t\t<vertex x=\"%f\" y=\"%f\" edge=\"%s\"/>"
+        woEdge = "\t\t<vertex x=\"%f\" y=\"%f\"/>"
+        for (x, y, upSide) in self.curBlockVertex:
+            if upSide == True:
                 if self.edgeTexture != '':
-                    self.content.append("\t\t<vertex x=\"%f\" y=\"%f\" edge=\"%s\"/>" % (x,-y,self.edgeTexture))
+                    self.content.append(wEdge % (x, -y, self.edgeTexture))
                 else:
-                    self.content.append("\t\t<vertex x=\"%f\" y=\"%f\"/>" % (x,-y))
+                    self.content.append(woEdge % (x, -y))
             else:
                 if self.downEdgeTexture != '':
-                    self.content.append("\t\t<vertex x=\"%f\" y=\"%f\" edge=\"%s\"/>" % (x,-y,self.downEdgeTexture))
+                    self.content.append(wEdge % (x, -y, self.downEdgeTexture))
                 else:
-                    self.content.append("\t\t<vertex x=\"%f\" y=\"%f\"/>" % (x,-y))
+                    self.content.append(woEdge % (x, -y))
 
         return ret
 
@@ -239,14 +241,14 @@ class Block(Element):
             angle = getValue(self.infos, 'edges', 'angle')
             if angle in [None, '270']:
                 tmpVertex = []        
-                firstVertice = self.currentBlockVertex[0]
+                firstVertice = self.curBlockVertex[0]
 
                 # add the first vertice so we can test the last one
-                self.currentBlockVertex.append(firstVertice)
+                self.curBlockVertex.append(firstVertice)
 
-                for i in xrange(len(self.currentBlockVertex)-1):
-                    x1,y1 = self.currentBlockVertex[i]
-                    x2,y2 = self.currentBlockVertex[i+1]
+                for i in xrange(len(self.curBlockVertex)-1):
+                    x1, y1 = self.curBlockVertex[i]
+                    x2, y2 = self.curBlockVertex[i+1]
 
                     if x1 == x2 and y1 == y2:
                         continue
@@ -258,15 +260,15 @@ class Block(Element):
                     else:
                         tmpVertex.append((x1, y1, False))
 
-                self.currentBlockVertex = tmpVertex
+                self.curBlockVertex = tmpVertex
             else:
                 tmpVertex = []        
-                firstVertice = self.currentBlockVertex[0]
-                self.currentBlockVertex.append(firstVertice)
+                firstVertice = self.curBlockVertex[0]
+                self.curBlockVertex.append(firstVertice)
 
-                for i in xrange(len(self.currentBlockVertex)-1):
-                    x1,y1 = self.currentBlockVertex[i]
-                    x2,y2 = self.currentBlockVertex[i+1]
+                for i in xrange(len(self.curBlockVertex)-1):
+                    x1, y1 = self.curBlockVertex[i]
+                    x2, y2 = self.curBlockVertex[i+1]
 
                     if x1 == x2 and y1 == y2:
                         continue
@@ -278,58 +280,51 @@ class Block(Element):
                     else:
                         tmpVertex.append((x1, y1, False))
 
-                self.currentBlockVertex = tmpVertex
+                self.curBlockVertex = tmpVertex
         elif drawmethod in ['in', 'out']:
-            self.currentBlockVertex = [(x, y, True) for (x, y) in self.currentBlockVertex]
+            self.curBlockVertex = [(x, y, True)
+                                       for (x, y) in self.curBlockVertex]
 
     def optimizeVertex(self):
-        def calculateAngleBetweenThreePoints(pt1, pt2, pt3):
-            x,y = range(2)
-            v1 = Vector(pt2[x]-pt1[x], pt2[y]-pt1[y])
-            v2 = Vector(pt3[x]-pt2[x], pt3[y]-pt2[y])          
+        def angleBetweenThreePoints(pt1, pt2, pt3):
+            v1 = Vector(pt2[0]-pt1[0], pt2[1]-pt1[1])
+            v2 = Vector(pt3[0]-pt2[0], pt3[1]-pt2[1])          
             angle = v1.angle(v2)
-            
             return angle
-
-        tmpVertex = []
-        nbVertexBefore = len(self.currentBlockVertex)
-        
-        tmpVertex.append(self.currentBlockVertex[0])
-        self.lastx = self.currentBlockVertex[0][0]
-        self.lasty = self.currentBlockVertex[0][1]
 
         def smooth2limit(smooth):
             # smooth [0 : 100] -> [0.1 : 0.001] * 100
             return (smooth * -0.00099 + 0.1) * 100
 
-        #xLimit = smooth2limit(self.smooth) * self.newWidth
-        #yLimit = smooth2limit(self.smooth) * self.newHeight
-        xLimit = smooth2limit(self.smooth)
-        yLimit = smooth2limit(self.smooth)
+        tmpVertex = []
+        tmpVertex.append(self.curBlockVertex[0])
+        lastX = self.curBlockVertex[0][0]
+        lastY = self.curBlockVertex[0][1]
+
+        limit = smooth2limit(self.smooth)
         angleLimit = 0.0314
-        for i in xrange(1, len(self.currentBlockVertex)-1):
-            x2,y2 = self.currentBlockVertex[i]
-            x3,y3 = self.currentBlockVertex[i+1]
-            angle = calculateAngleBetweenThreePoints((self.lastx,self.lasty), (x2,y2), (x3,y3))
-            if ((abs(x2 - self.lastx) > xLimit or abs(y2 - self.lasty) > yLimit) and abs(angle) > angleLimit) or (abs(angle) > angleLimit*10):
+        for i in xrange(1, len(self.curBlockVertex)-1):
+            x2, y2 = self.curBlockVertex[i]
+            x3, y3 = self.curBlockVertex[i+1]
+            angle = angleBetweenThreePoints((lastX, lastY),
+                                            (x2, y2),
+                                            (x3, y3))
+            if (((abs(x2 - lastX) > limit or abs(y2 - lastY) > limit)
+                 and abs(angle) > angleLimit)
+                or (abs(angle) > angleLimit*10)):
                 tmpVertex.append((x2, y2))
-                self.lastx = x2
-                self.lasty = y2
+                lastX = x2
+                lastY = y2
                 Stats().addOptimizedVertice(self.curBlock)
 
-        tmpVertex.append(self.currentBlockVertex[-1])
-        self.currentBlockVertex = tmpVertex
-        nbVertexAfter = len(self.currentBlockVertex)
-        
-        logging.debug("optimizeVertex[%s]:: %d vertex -> %d vertex" % (self.curBlock,
-                                                                       nbVertexBefore,
-                                                                       nbVertexAfter))
+        tmpVertex.append(self.curBlockVertex[-1])
+        self.curBlockVertex = tmpVertex
 
     def addVertice(self, x, y):
         # we change the block pos
         x = x - self.xDiff
         y = y + self.yDiff
-        self.currentBlockVertex.append((x, y))
+        self.curBlockVertex.append((x, y))
         self.aabb.addPoint(x, y)
         Stats().addVertice(self.curBlock)
 
@@ -337,12 +332,13 @@ class Block(Element):
         """
         http://astronomy.swin.edu.au/~pbourke/geometry/clockwise/
         """
-        length = len(self.currentBlockVertex)
+        length = len(self.curBlockVertex)
         if length < 2:
             return
 
         # put the block in his own space
-        translatedVertex = [(x-self.aabb.x(), y-self.aabb.y()) for x, y in self.currentBlockVertex]
+        translatedVertex = [(x-self.aabb.x(), y-self.aabb.y())
+                            for x, y in self.curBlockVertex]
 
         area = 0
         for i in range(len(translatedVertex)-1):
@@ -351,12 +347,7 @@ class Block(Element):
             area += x1 * (-y2) - x2 * (-y1)
 
         if area > 0:
-            logging.debug("reverse the block %s. block area: %f"
-                          % (self.curBlock, area))
-            self.currentBlockVertex.reverse()
-        else:
-            logging.debug("block not reverse %s. block area: %f"
-                          % (self.curBlock, area))
+            self.curBlockVertex.reverse()
 
         # use the area to put the block mass for chipmunk
         self.mass = fabs(area)
